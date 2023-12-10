9f478e084927c502d5e51cce437c94735a20c187
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2023-12-10 14:34:19 +0530

    #commit 3

--- Prasang-2/prasang/src/app/Components/Navbar/Navbar.js
+++ Prasang-2/prasang/src/app/Components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <div>
-        <div class="navbar">
-        <h1 class="logo-top">Prasang</h1>
-        <div class="navigation">
+        <div className="navbar">
+        <h1 className="logo-top">Prasang</h1>
+        <div className="navigation">
             <li>Home</li>
             <li>Workshops</li>
             <li>Competitons</li>
--- Prasang-2/prasang/src/app/page.js
+++ Prasang-2/prasang/src/app/page.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from './Components/Navbar/Navbar'
+import HOME from './Components/Home/Home'
+
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Navbar></Navbar>
+      <HOME></HOME>
+
     </main>
   )
 }
